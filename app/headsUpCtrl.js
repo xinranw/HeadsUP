0261c03f0a323763a5a4c4a610e702049518f99c
Xinran Wang <xw1992@gmail.com>
2015-06-06 15:44:32 +0100

    Reorganized. Added bootstrap and angular.

---
+++ app/headsUpCtrl.js
@@ -0,0 +1,29 @@
+angular.module('app').controller('headsUpCtrl', function ($scope) {
+
+    // $scope.$watch('todoStorage.data', function() {
+    //     $scope.todoList = $scope.todoStorage.data;
+    // });
+
+    // $scope.todoStorage.findAll(function(data){
+    //     $scope.todoList = data;
+    //     $scope.$apply();
+    // });
+
+    // $scope.add = function() {
+    //     todoStorage.add($scope.newContent);
+    //     $scope.newContent = '';
+    // }
+
+    // $scope.remove = function(todo) {
+    //     todoStorage.remove(todo);
+    // }
+
+    // $scope.removeAll = function() {
+    //     todoStorage.removeAll();
+    // }
+
+    // $scope.toggleCompleted = function() {
+    //     todoStorage.sync();
+    // }
+
+});
--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   var btn = document.createElement("button");
   var t = document.createTextNode("Heads up!");
   btn.appendChild(t);
-  btn.classList.add('hu-button');
+  btn.classList.add('hu-button', 'btn', 'btn-primary');
   //Appending to DOM 
   // document.body.appendChild(btn);
   // console.log('here');
-  var watchHeader = document.getElementById('watch-header');
-  watchHeader.appendChild(btn);
+  var titleContainer = document.getElementsByClassName('watch-title-container')[0];
+  titleContainer.appendChild(btn);
   
   btn.addEventListener('click', function() {
     alert('added');
